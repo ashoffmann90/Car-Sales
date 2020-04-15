a0a04fb192160212ab00d66b59020d62c81b376f
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-04-14 17:07:06 -0700

    fnished MVP

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 import AddedFeature from './AddedFeature';
+import {removeFeature} from '../actions/featureActions'
 
 const AddedFeatures = props => {
+
+  const removeItem = item => {
+    props.removeFeature(item)
+  }
+
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature delete={removeItem} key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
@@ -19,4 +25,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = state => {
+  return {
+    car: state.car
+  }
+}
+
+export default connect(mapStateToProps, {removeFeature})(AddedFeatures);
